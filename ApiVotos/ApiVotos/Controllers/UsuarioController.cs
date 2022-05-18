31ffdb1fc5c06add8de348ca9d7ee6eac6e58976
Camilo <cgcamilogomez@gmail.com>
2022-05-17 21:49:45 -0500

    Se agrega decorador EnableCors

--- ApiVotos/ApiVotos/Controllers/UsuarioController.cs
+++ ApiVotos/ApiVotos/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Modelos;
 using Negocio.Interfaces;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace ApiVotos.Controllers
 {
+    [EnableCors]
     [Route("api/[controller]")]
     [ApiController]
     public class UsuarioController : ControllerBase
